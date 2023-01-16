8f30c30298643721521a2ca670365a2a49691aff
tzztson <zhenghu61919@gmail.com>
2023-01-16 21:55:14 +0800

    wagmi connect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { createClient, WagmiConfig } from "wagmi";
+import { connectors, provider } from "./wagmi";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const client = createClient({
+    autoConnect: true,
+    connectors: connectors,
+    provider,
+  });
+  return (
+    <WagmiConfig client={client}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  );
 }
